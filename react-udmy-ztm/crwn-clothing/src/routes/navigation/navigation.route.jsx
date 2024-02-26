671da9c23fb295fc62e052aa1342a2598fab9faf
Rahul Saini <rahkds@gmail.com>
2024-02-26 16:10:17 +0530

    adding cart context, shopping icon, cart dropdown etc.

--- react-udmy-ztm/crwn-clothing/src/routes/navigation/navigation.route.jsx
+++ react-udmy-ztm/crwn-clothing/src/routes/navigation/navigation.route.jsx
@@ -4,10 +4,14 @@ import './navigation.styles.scss';
 import { UserContext } from "../../contexts/user.context";
 import { useContext } from "react";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropDown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
+
 
 const Navigation= () => {
     const {currentUser} = useContext(UserContext);
-
+    const {isCartOpen} = useContext(CartContext);
     const signOutHandler = async () => {
         await signOutUser();
     }
@@ -31,8 +35,9 @@ const Navigation= () => {
                         </Link>   
                     )
                 }
-             
+                <CartIcon/>
             </div>
+            {isCartOpen && <CartDropDown/>}
         </div>
         <Outlet/>
       </>
