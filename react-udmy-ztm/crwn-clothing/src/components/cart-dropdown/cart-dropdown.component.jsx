f05f8a955733ff331a25682725fa26f34912dd54
Rahul Saini <rahkds@gmail.com>
2024-02-26 17:33:11 +0530

    added cart-item and count functionality

--- react-udmy-ztm/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ react-udmy-ztm/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,21 @@
 
+import { useContext } from 'react';
 import Button from '../button/button.component';
 import './cart-dropdown.styles.scss'
-
+import { CartContext } from '../../contexts/cart.context';
+import CartItem from '../cart-item/cart-item.component';
 
 const CartDropDown = () => {
+    const {cartItems} = useContext(CartContext);
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items' />
+            <div className='cart-items' >
+                {cartItems.map((item) => {
+                    return (
+                        <CartItem key={item.id} cartItem={item}/>
+                    );
+                })}
+            </div>
             <Button>GO THE CHECKOUT</Button>
         </div>
     );
