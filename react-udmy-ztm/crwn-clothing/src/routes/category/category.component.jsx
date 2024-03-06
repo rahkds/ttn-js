2e88d5f8032ebfc924469710e9bfc52d5df8a948
Rahul Saini <rahkds@gmail.com>
2024-03-06 10:45:29 +0530

    add redux-thunk, section 16

--- react-udmy-ztm/crwn-clothing/src/routes/category/category.component.jsx
+++ react-udmy-ztm/crwn-clothing/src/routes/category/category.component.jsx
@@ -6,13 +6,14 @@ import ProductCard from '../../components/product-card/product-card.component';
 
 import './category.styles.scss';
 import { useSelector } from 'react-redux';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
-
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
+import Spinner from '../../components/spinner/spinner.component';
 
 const Category = () => {
   const { category } = useParams();
   console.log("category render");
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -23,12 +24,18 @@ const Category = () => {
   return (
     <Fragment>
       <h2 className='category-title'>{category.toUpperCase()}</h2>
-      <div className='category-container'>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </div>
+
+      {
+        isLoading ? (<Spinner />) : (
+          <div className='category-container'>
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+          </div>
+        )
+      }
+
     </Fragment>
   );
 };
--- react-udmy-ztm/crwn-clothing/src/store/store.js
+++ react-udmy-ztm/crwn-clothing/src/store/store.js
@@ -4,6 +4,7 @@ import { rootReducer } from './root-reducer';
 import {persistStore, persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { loggerMiddleware } from './middleware/logger';
+import {thunk} from 'redux-thunk';
 
 const persistConfig = {
     key : 'root',
@@ -13,7 +14,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [(process.env.NODE_ENV !== 'production' ? loggerMiddleware : false)].filter(Boolean);
+const middleWares = [(process.env.NODE_ENV !== 'production' ? loggerMiddleware : false), thunk].filter(Boolean);
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
