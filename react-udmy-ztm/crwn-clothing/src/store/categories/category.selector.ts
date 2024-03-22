ff600531d29cf7b3e9fd755f46a1892f781ef41c
Rahul Kumar Saini <rahkds@gmail.com>
2024-03-22 17:32:57 +0530

    start crwn-clothing convert to typescript

--- react-udmy-ztm/crwn-clothing/src/store/categories/category.selector.ts
+++ react-udmy-ztm/crwn-clothing/src/store/categories/category.selector.ts
@@ -1,6 +1,9 @@
 import {createSelector} from 'reselect';
+import { CategoriesState } from './category.reducer';
+import {CategoryMap} from './category.types';
 
-const selectCategoryReducer = (state) => state.categories;
+
+const selectCategoryReducer = (state) : CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
@@ -15,7 +18,7 @@ export const selectCategoriesMap = createSelector(
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
         return acc;
-      }, {});
+      }, {} as CategoryMap);
     }
 )
 
