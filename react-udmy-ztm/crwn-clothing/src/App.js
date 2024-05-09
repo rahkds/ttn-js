0590048df8a12de584113122fd8f38ebc312c867
Rahul Kumar Saini <rahkds@gmail.com>
2024-05-09 14:22:52 +0530

    optimization letcure, section 27 completed

--- react-udmy-ztm/crwn-clothing/src/App.js
+++ react-udmy-ztm/crwn-clothing/src/App.js
@@ -1,13 +1,31 @@
-import Home from "./routes/home/home.route";
+// import Home from "./routes/home/home.route";
+import React from "react";
 import {Routes, Route, Outlet} from 'react-router-dom';
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from './routes/authentication/authentication.component';
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./components/checkout/checkout.component";
+// import Navigation from "./routes/navigation/navigation.component";
+// import Authentication from './routes/authentication/authentication.component';
+// import Shop from "./routes/shop/shop.component";
+// import Checkout from "./components/checkout/checkout.component";
+
+
+// import Navigation from "./routes/navigation/navigation.component";
+// import Authentication from './routes/authentication/authentication.component';
+// import Shop from "./routes/shop/shop.component";
+// import Checkout from "./components/checkout/checkout.component";
+
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "./utils/firebase/firebase.utils";
 import { checkUserSession } from "./store/user/user.action";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
+import Spinner from "./components/spinner/spinner.component";
+
+const Home = React.lazy(() => import('./routes/home/home.route'));
+const Navigation = React.lazy(() => import("./routes/navigation/navigation.component"));
+const Authentication = React.lazy(() => import('./routes/authentication/authentication.component'));
+const Shop = React.lazy(() => import("./routes/shop/shop.component"));
+const Checkout = React.lazy(() => import("./components/checkout/checkout.component"));
+
+
+
 
 const App = () => {
 
@@ -25,14 +43,17 @@ const App = () => {
 }, [])
 
  return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <React.Suspense fallback={<Spinner/>}>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </React.Suspense>
+
   );
 }
 
